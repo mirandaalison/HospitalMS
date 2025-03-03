d9468df76ffca2cf0637981b25413730ccc838d3
Miranda Creamer Alison Solange <mirandacreamer@gmail.com>
2025-03-03 18:40:33 -0500

    Pacientes completo sin modal agregar

--- HospitalMS/CapaEntidad/PacientesCLS.cs
+++ HospitalMS/CapaEntidad/PacientesCLS.cs
@@ -8,12 +8,12 @@ namespace CapaEntidad
 {
     public class PacientesCLS
     {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public DateTime FechaNacimiento { get; set; }
-        public string Telefono { get; set; }
-        public string Email { get; set; }
-        public string Direccion { get; set; }
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+        public string direccion { get; set; }
     }
 }
